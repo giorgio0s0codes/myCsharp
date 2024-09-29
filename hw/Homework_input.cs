c9b8e775d5026ce9eaed5111ef9aa37c354129a5
giorgio0s0codes <giorgio.oso@outlook.com>
2024-09-29 16:28:30 -0600

    Trying to include .sln file

---
+++ hw/Homework_input.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Read name, surname, age, weight (in kg) and height (in cm) from console.
+            Print a message to the screen based on the information given in 1. (an example message is shown below):
+
+            Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm. 
+
+            Calculate and print body-mass index (BMI)
+            Do 1 and 2 for another person.
+            */
+
+            ReceiveData();
+        }
+
+        static string ReceiveData()
+        {
+            string data;
+
+        }
+    }
+}
